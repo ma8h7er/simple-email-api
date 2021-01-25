2d53a6622e3c7b99a6c3a4a2e294e6071233bb3f
Maher Alhamod <ma8h7er@gmail.com>
2021-01-25 16:44:46 -0500

    setup send route and email controller

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class EmailController extends Controller
+{
+    /**
+     * Send array of emails, request must contains array of email arrays,
+     * each email array contains (recipient email address, email subject, email body, array of attachments)
+     * each attachment contains the name of file and the file content as base64
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function send(Request $request)
+    {
+        try {
+            
+            return response()->json([
+                'message' => 'Emails queued successfully.'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getTrace()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('api.token')
+    ->post('/send', [
+        'as' => 'api.email.send',
+        'uses' => 'EmailController@send'
+    ]);
